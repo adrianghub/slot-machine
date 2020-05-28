f54d3af0ec36c2eab0bc99a3e236cf1e675d4bfb
Adrian <adrian_zinko@op.pl>
2020-05-29 01:15:24 +0200

    feat: add statistics class based component

---
+++ src/js/Statistics.js
@@ -0,0 +1,25 @@
+// Statistics 
+
+
+export default class Statistics {
+    constructor() {
+      this.gameResults = [];
+    }
+    
+    attachGameToStatistics(win, bid) {
+      let gameResult = {
+        win,
+        bid
+      }
+      console.log(gameResult);
+      this.gameResults.push(gameResult);
+    }
+    
+    showGameStatistics() {
+      let games = this.gameResults.length;
+      let wins = this.gameResults.filter(result => result.win).length;
+      let losses = this.gameResults.filter(result => !result.win).length;
+      return [games, wins, losses];
+    }
+    
+  }
